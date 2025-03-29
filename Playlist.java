9c4c585dafef1e6df36ba61ba83237a5a97adbd2
Zuriel Ali Sampé Sánchez <sampesanchezzurielali@gmail.com>
2025-03-28 23:51:36 -0600

    Add files via upload

---
+++ Playlist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+      Musicas P1=new Musicas("Junio H","Se Quedan Viendo","2020",4);
+      P1.ShowPlaylist();
+    }
+}
